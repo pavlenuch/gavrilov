196420a92072742c557d83708c44aeffd920f31a
Sashko <pavlenuch@yandex.by>
2021-04-25 12:53:15 +0300

    проект создан

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React/*, { useState }*/ from "react"
+import IconsLink from "../IconsLink/IconsLink"
+import './header.css'
+
+const Header = () => {
+
+    return (
+        <header className='header'>
+            <IconsLink />
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/IconsLink/IconsLink.jsx
@@ -0,0 +1,13 @@
+import React/*, { useState }*/ from "react"
+import './IconsLink.css'
+
+const IconsLink = () => {
+
+    return (
+        <div className='wrap_icons-link'>
+
+        </div>
+    )
+}
+
+export default IconsLink
